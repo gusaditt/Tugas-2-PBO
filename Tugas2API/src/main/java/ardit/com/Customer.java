192ef71ac6ba7064a5434949b877801f80dd26ca
Arya Ekananda <147296239+AryaEkanandaa@users.noreply.github.com>
2024-06-20 16:34:23 +0800

    Update Customer.java

--- Tugas2API/src/main/java/ardit/com/Customer.java
+++ Tugas2API/src/main/java/ardit/com/Customer.java
@@ -1,6 +1,60 @@
 package ardit.com;
 
 public class Customer {
+    // Variables
+    private int id;
+    private String email;
+    private String first_name;
+    private String last_name;
+    private String phone_number;
 
+    public Customer() {
+        this.id = id;
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone_number = phone_number;
+    }
+
+    // Getter and Setter methods
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
 }
 
