087037f68b5be04d435adc45fbf956424f98e042
Arya Ekananda <147296239+AryaEkanandaa@users.noreply.github.com>
2024-06-20 16:37:18 +0800

    Update subscriptionItems.java

--- Tugas2API/src/main/java/ardit/com/subscriptionItems.java
+++ Tugas2API/src/main/java/ardit/com/subscriptionItems.java
@@ -1,5 +1,62 @@
 package ardit.com;
 
 public class subscriptionItems {
+    // Variables
+    private int subscription;
+    private int item;
+    private int quantity;
+    private int price;
+    private int amount;
 
+    // Constructors
+    public subscriptionItems() {}
+
+    public subscriptionItems(int subscription, int item, int quantity, int price, int amount) {
+        this.subscription = subscription;
+        this.item = item;
+        this.quantity = quantity;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    // Getter and Setter methods
+    public int getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(int subscription) {
+        this.subscription = subscription;
+    }
+
+    public int getItem() {
+        return item;
+    }
+
+    public void setItem(int item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
