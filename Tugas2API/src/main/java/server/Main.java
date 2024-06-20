8fc69aa4bae3fe94eec4de5059ae3b0b95a296b2
gusaditt <146801690+gusaditt@users.noreply.github.com>
2024-06-21 01:24:03 +0800

    Update Main.java

--- Tugas2API/src/main/java/server/Main.java
+++ Tugas2API/src/main/java/server/Main.java
@@ -6,13 +6,11 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
-import static sun.net.www.protocol.http.AuthCacheValue.Type;
-
+import com.sun.net.httpserver.HttpServer;
 public class Main {
     public static void main(String[] args) throws IOException{
         try {
             HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 9057), 0);
-            // context untuk endpoint "", dgn class untuk handle HTTP RequestHandler
             httpServer.createContext("/", new Server());
             httpServer.setExecutor(Executors.newSingleThreadExecutor());
             httpServer.start();
