1ffb53fc9eb50663f5693e2226000f0b620a7bb4
Arya Ekananda <147296239+AryaEkanandaa@users.noreply.github.com>
2024-06-21 08:57:21 +0800

    Update subscriptionsData.java

--- Tugas2API/src/main/java/persistences/subscriptionsData.java
+++ Tugas2API/src/main/java/persistences/subscriptionsData.java
@@ -1,5 +1,7 @@
 package persistences;
 
+
+import ardit.com.Customer;
 import ardit.com.Subscriptions;
 
 import java.sql.*;
@@ -7,9 +9,85 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class subscriptionsData {
-// DateTimeFormatter for parsing and formatting date-time strings
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    // DateTimeFormatter for parsing and formatting date-time strings
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    //SELECT ALL SUBSCRIPTIONS
+    public ArrayList<Subscriptions> selectAllSubscriptions() throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        ArrayList<Subscriptions> listsubscriptions = new ArrayList<>();
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:subscription.db");
+            System.out.println("Connected to database");
+
+            statement = connection.prepareStatement("SELECT * FROM subscriptions");
+            result = statement.executeQuery();
+
+            while (result.next()) {
+                Subscriptions subscriptions = new Subscriptions();
+                subscriptions.setId(result.getInt("id"));
+                subscriptions.setCustomer(result.getInt("customer"));
+                subscriptions.setBilling_period(result.getInt("billing_period"));
+                subscriptions.setBilling_period_unit(result.getString("billing_period_unit"));
+                subscriptions.setTotal_due(result.getInt("total_due"));
+                subscriptions.setActivated_at(result.getInt("actived_at"));
+                subscriptions.setCurrent_term_start(result.getString("current_term_start"));
+                subscriptions.setCurrent_term_end(result.getString("current_term_end"));
+                subscriptions.setStatus(result.getString("status"));
+                listsubscriptions.add(subscriptions);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            throw new RuntimeException(e);
+        } finally {
+            if (result != null) result.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        }
+        return listsubscriptions;
+    }
+    //SELECT SORT SUBSCRIPTIONS
+    public ArrayList<Subscriptions> selectSortSubscription() throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        ArrayList<Subscriptions> listsubscriptions = new ArrayList<>();
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:subscription.db");
+            System.out.println("Connected to database");
+
+            statement = connection.prepareStatement("SELECT * FROM subscriptions ORDER BY current_term_end DESC");
+            result = statement.executeQuery();
+
+            while (result.next()) {
+                Subscriptions subscriptions = new Subscriptions();
+                subscriptions.setId(result.getInt("id"));
+                subscriptions.setCustomer(result.getInt("customer"));
+                subscriptions.setBilling_period(result.getInt("billing_period"));
+                subscriptions.setBilling_period_unit(result.getString("billing_period_unit"));
+                subscriptions.setTotal_due(result.getInt("total_due"));
+                subscriptions.setActivated_at(result.getInt("actived_at"));
+                subscriptions.setCurrent_term_start(result.getString("current_term_start"));
+                subscriptions.setCurrent_term_end(result.getString("current_term_end"));
+                subscriptions.setStatus(result.getString("status"));
+                listsubscriptions.add(subscriptions);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            throw new RuntimeException(e);
+        } finally {
+            if (result != null) result.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        }
+        return listsubscriptions;
+    }
     // SELECT SUBSCRIPTIONS BY CUSTOMER ID
     public ArrayList<Subscriptions> selectSubscriptionsByCustomer(int idCustomer) throws SQLException {
         Connection connection = null;
@@ -20,7 +98,7 @@ public class subscriptionsData {
         try {
             Class.forName("org.sqlite.JDBC");
             // Establish connection to SQLite database
-            connection = DriverManager.getConnection("jdbc:sqlite:ecommerce.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:subscription.db");
             System.out.println("Connected to database");
             statement = connection.prepareStatement("SELECT * FROM subscriptions WHERE customer = ?");
             statement.setInt(1, idCustomer);
@@ -33,7 +111,7 @@ public class subscriptionsData {
                 subscription.setBilling_period(result.getInt("billing_period"));
                 subscription.setBilling_period_unit(result.getString("billing_period_unit"));
                 subscription.setTotal_due(result.getInt("total_due"));
-                subscription.setActivated_at(result.getString("activated_at"));
+                subscription.setActivated_at(result.getInt("actived_at"));
                 subscription.setCurrent_term_start(result.getString("current_term_start"));
                 subscription.setCurrent_term_end(result.getString("current_term_end"));
                 subscription.setStatus(result.getString("status"));
@@ -60,7 +138,7 @@ public class subscriptionsData {
         try {
             Class.forName("org.sqlite.JDBC");
             // Establish connection to SQLite database
-            connection = DriverManager.getConnection("jdbc:sqlite:ecommerce.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:subscription.db");
             System.out.println("Connected to database");
             statement = connection.prepareStatement("SELECT * FROM subscriptions WHERE customer = ? AND status = ?");
             statement.setInt(1, idCustomer);
@@ -74,7 +152,7 @@ public class subscriptionsData {
                 subscription.setBilling_period(result.getInt("billing_period"));
                 subscription.setBilling_period_unit(result.getString("billing_period_unit"));
                 subscription.setTotal_due(result.getInt("total_due"));
-                subscription.setActivated_at(result.getString("activated_at"));
+                subscription.setActivated_at(result.getInt("actived_at"));
                 subscription.setCurrent_term_start(result.getString("current_term_start"));
                 subscription.setCurrent_term_end(result.getString("current_term_end"));
                 subscription.setStatus(result.getString("status"));
@@ -107,9 +185,9 @@ public class subscriptionsData {
             statement.setInt(2, subscription.getBilling_period());
             statement.setString(3, subscription.getBilling_period_unit().toString());
             statement.setInt(4, subscription.getTotal_due());
-            statement.setString(5, subscription.getActivated_at().format(formatter));
-            statement.setString(6, subscription.getCurrent_term_start().format(formatter));
-            statement.setString(7, subscription.getCurrent_term_end().format(formatter));
+            statement.setInt(5, subscription.getActivated_at());
+            statement.setString(6, subscription.getCurrent_term_start());
+            statement.setString(7, subscription.getCurrent_term_end());
             statement.setString(8, subscription.getStatus().toString());
 
             int rowsAffected = statement.executeUpdate();
@@ -146,9 +224,9 @@ public class subscriptionsData {
             statement.setInt(1, subscription.getBilling_period());
             statement.setString(2, subscription.getBilling_period_unit().toString());
             statement.setInt(3, subscription.getTotal_due());
-            statement.setString(4, subscription.getActivated_at().format(formatter));
-            statement.setString(5, subscription.getCurrent_term_start().format(formatter));
-            statement.setString(6, subscription.getCurrent_term_end().format(formatter));
+            statement.setInt(4, subscription.getActivated_at());
+            statement.setString(5, subscription.getCurrent_term_start());
+            statement.setString(6, subscription.getCurrent_term_end());
             statement.setString(7, subscription.getStatus().toString());
             statement.setInt(8, idSubscription);
 
