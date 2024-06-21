cfe9d6ccba2bd00357be846b98b7c7d6190db975
gusaditt <146801690+gusaditt@users.noreply.github.com>
2024-06-21 20:46:20 +0800

    Update CardsRequest.java

--- Tugas2API/src/main/java/server/CardsRequest.java
+++ Tugas2API/src/main/java/server/CardsRequest.java
@@ -1,4 +1,16 @@
 package server;
 
+import persistences.*;
+
+import java.sql.*;
+
+
 public class CardsRequest {
+    CardsData cardsData = new CardsData();
+    public String deleteCards(String[] path) throws SQLException {
+        int customerId = Integer.parseInt(path[2]);
+        int idCard = Integer.parseInt(path[4]);
+
+        return cardsData.deleteCard(customerId, idCard);
+    }
 }
